a154e1f6f95b39d975553137d36ef6642f18944d
kp <mavericksstyle25@gmail.com>
2025-06-24 16:04:40 -0700

    added logic for reading document

---
+++ main.py
@@ -0,0 +1,33 @@
+import os
+import sys
+import pymupdf
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+
+sys.path.append(parent_dir)
+
+from dspy_module.doc_dspy import dspy_doc
+
+
+def extract_text_from_pdf(file):
+    """
+    Extract text from a PDF file.
+    """
+    doc_text=[]
+    try:
+        with pymupdf.open(file) as doc:
+            for page in doc:
+                doc_text.append(page.get_text())
+        return doc_text
+    except Exception as e:
+        print(f "Error extracting text from PDF: {e}")
+        return None
+
+
+Doc_Identifier= dspy_doc()
+
+ds="dataset/The_Metamorphosis_Franz_Kafka.pdf"
+data= extract_text_from_pdf(ds)
+
+
